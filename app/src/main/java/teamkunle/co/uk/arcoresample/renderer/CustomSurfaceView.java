61a2dc26dc9152d30460cf0e5b019d81d65db467
Kunle Ogunjimi <kunle.ogunjimi@telegraph.co.uk>
2017-10-13 15:56:21 +0100

    initial creation of custom helper views

---
+++ app/src/main/java/teamkunle/co/uk/arcoresample/renderer/CustomSurfaceView.java
@@ -0,0 +1,23 @@
+package teamkunle.co.uk.arcoresample.renderer;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class CustomSurfaceView extends GLSurfaceView {
+
+    private final int EGL_CLIENT = 0;
+    private GLSurfaceView.Renderer renderer;
+
+
+    public CustomSurfaceView(Context context, GLSurfaceView.Renderer renderer) {
+        super(context);
+        this.renderer = renderer;
+        initViews();
+    }
+
+    private void initViews() {
+        setEGLContextClientVersion(EGL_CLIENT);
+        setRenderer(renderer);
+    }
+
+}
