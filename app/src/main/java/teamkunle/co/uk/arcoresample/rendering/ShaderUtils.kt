081b8d4ef6475bc3f228939057b6da559380479c
Kunle Ogunjimi <kunle.ogunjimi@telegraph.co.uk>
2017-12-03 22:08:30 +0000

    code clean up in shader utils class

--- app/src/main/java/teamkunle/co/uk/arcoresample/rendering/ShaderUtils.kt
+++ app/src/main/java/teamkunle/co/uk/arcoresample/rendering/ShaderUtils.kt
@@ -3,9 +3,15 @@ package teamkunle.co.uk.arcoresample.rendering
 import android.content.Context
 import android.opengl.GLES20
 import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 
 class ShaderUtils {
     companion object {
+
+        //load shader file
         @JvmStatic
         fun loadGlShader(tag : String, context : Context,type : Int ,resId : Int) : Int {
             val code  = readRawTextFile(context, resId)
@@ -29,9 +35,33 @@ class ShaderUtils {
             return shader
         }
 
+        //check if we have an error in Open GL ES
+        //TODO : cos you cannot do assignment in kotlin redo this code
+        @JvmStatic
+        fun checkGLError(tag : String, label : String) {
+            var error = GLES20.glGetError()
+
+            while (error != GLES20.GL_NO_ERROR) {
+                Log.e(tag, "$label : glError $error")
+                throw RuntimeException("$label : glError $error")
+            }
+        }
+
+        //read the raw file
         @JvmStatic
         fun readRawTextFile(context: Context, resId : Int) : String {
+            var inputStream : InputStream = context.resources.openRawResource(resId)
+            try {
+                val reader : BufferedReader = BufferedReader(InputStreamReader(inputStream))
+                val sb   : StringBuilder
+                val line : StringBuilder
 
+
+                reader.close()
+
+            } catch (e : IOException) {
+
+            }
             return ""
         }
     }
