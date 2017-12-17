e213ba294b2cf4305a41883bc26d8548e30751a7
Kunle Ogunjimi <kunle.ogunjimi@telegraph.co.uk>
2017-12-17 23:22:24 +0000

    Object Renderer work started

---
+++ app/src/main/java/teamkunle/co/uk/arcoresample/rendering/ObjectRenderer.kt
@@ -0,0 +1,13 @@
+package teamkunle.co.uk.arcoresample.rendering
+
+/**
+ * Renders an object loaded from an obj file in OpenGL
+ */
+
+class ObjectRenderer {
+
+    companion object {
+        private const val TAG = "ObjectRenderer"
+    }
+
+}
