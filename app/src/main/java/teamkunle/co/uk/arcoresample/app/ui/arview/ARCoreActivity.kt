3f2656ccae82e813ada41b7f0b31c0cb4f5684e5
Kunle Ogunjimi <kunle.ogunjimi@telegraph.co.uk>
2018-02-18 22:14:11 +0000

    ready to merge before refactor

--- app/src/main/java/teamkunle/co/uk/arcoresample/app/ui/arview/ARCoreActivity.kt
+++ app/src/main/java/teamkunle/co/uk/arcoresample/app/ui/arview/ARCoreActivity.kt
@@ -1,6 +1,7 @@
 package teamkunle.co.uk.arcoresample.app.ui.arview
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_arcore.*
 import teamkunle.co.uk.arcoresample.R
 import teamkunle.co.uk.arcoresample.app.ui.base.BaseActivity
 import teamkunle.co.uk.arcoresample.utility.PermissionUtils
@@ -11,7 +12,16 @@ class ARCoreActivity : BaseActivity(), ARCoreView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        presenter.attachView(this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (PermissionUtils.hasCameraPermission(this)) {
+            custom_surface_view.onResume()
+        } else {
+            PermissionUtils.requestCameraPermission(this)
+        }
     }
 
     override fun onDestroy() {
@@ -19,7 +29,16 @@ class ARCoreActivity : BaseActivity(), ARCoreView {
         super.onDestroy()
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (!PermissionUtils.hasCameraPermission(this)) {
+            if (!PermissionUtils.shouldShowRequestPermissionRationale(this)) {
+                PermissionUtils.launchPermissionSettings(this)
+            }
+        }
+    }
+
     override fun initView() {
+        presenter.attachView(this)
     }
 
     override fun getLayOutResourcedId(): Int {
@@ -29,4 +48,6 @@ class ARCoreActivity : BaseActivity(), ARCoreView {
     override fun permissionCheckCamera() {
         PermissionUtils.requestCameraPermission(this)
     }
+
+
 }
--- app/src/main/java/teamkunle/co/uk/arcoresample/app/ui/arview/ARCoreView.kt
+++ app/src/main/java/teamkunle/co/uk/arcoresample/app/ui/arview/ARCoreView.kt
@@ -3,6 +3,5 @@ package teamkunle.co.uk.arcoresample.app.ui.arview
 import teamkunle.co.uk.arcoresample.app.ui.base.BaseView
 
 interface ARCoreView : BaseView {
-
-    fun permissionCheckCamera()
+     fun permissionCheckCamera()
 }
